79c3de475de970fa30a1e2a9ddae8113ab15e587
njeruthuo <juliusn411@gmail.com>
2025-08-12 18:46:34 +0300

    alembic init

--- TodoApp/models.py
+++ TodoApp/models.py
@@ -12,6 +12,7 @@ class Users(Base):
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
     role = Column(String)
+    # phone = Column(String)
 
 
 class Todos(Base):
